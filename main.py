53d9ead6f86929331cb1c07afcd5c343ee6501d8
YasinUottawa <yasinelmi100@gmail.com>
2022-02-19 21:33:51 -0500

    changed

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800,600))
 
-#
-
+#Title and Icon
+pygame.display.set_caption("Online School Simulator")
+#icon = pygame.image.load("")
+#pygame.display.set_icon(icon)
+maxFocus = 500
+focus = 0
 #Game Loop
 running=True
 while running:
@@ -14,4 +18,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.fill((255,0,0))
+    pygame.display.update()
 
