97af51c5dee8e4d230297913e5615acff3bc09eb
YasinUottawa <yasinelmi100@gmail.com>
2022-02-19 21:17:28 -0500

    initial push

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+ 
+
+screen = pygame.display.set_mode((800,600))
+
+
+
+#Game Loop
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
